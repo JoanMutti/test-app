789d317fc5697ff17504eeacadacb254d854f734
JoanMutti <joanmutti1090@gmail.com>
2021-04-13 09:14:32 -0300

    fin clase 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ListOfGifs from "./components/ListOfGifs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Link, Route } from "wouter";
+
+const App = () => {
+	return (
+		<div className="App">
+			<section className="App-content">
+				<h1>App</h1>
+				<Link to="/search/panda">Gifs de pandas</Link>
+				<Link to="/search/morty">Gifs de morty</Link>
+				<Link to="/search/rick">Gifs de rick</Link>
+				<Route path="/search/:keyword" component={ListOfGifs} />
+			</section>
+		</div>
+	);
+};
 
 export default App;
